91b842d50949ad99c38ac49777cb3054ffcf7db1
Happy Hee <56466589+happy-hee@users.noreply.github.com>
2024-06-27 08:50:30 +0900

    첫번째 앱 만들기 - UI설정하기 (앱7강)

---
+++ FirstNewApp/FirstNewApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  FirstNewApp
+//
+//  Created by 김다희 on 6/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+    
+}
+
