98d630fc2e616230bd13182ed5db3b30c1430b54
Happy Hee <56466589+happy-hee@users.noreply.github.com>
2024-06-30 15:46:12 +0900

    첫번째 앱 만들기 - 코드 설정하기 (앱10강)

--- FirstNewApp/FirstNewApp/ViewController.swift
+++ FirstNewApp/FirstNewApp/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation // 시스템 소리를 내게 하기 위해 추가한 프레임워크
 
 class ViewController: UIViewController {
 
@@ -13,7 +14,9 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var slider: UISlider!
     
+    weak var timer: Timer?
     
+    var number = 3
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,18 +41,52 @@ class ViewController: UIViewController {
         // 2) 너무 디테일한 값으로 되어있으므로 정수(Int) 형태로 변형
         let seconds = Int(slider.value * 60)
         
-        mainLabel.text = "\(seconds)"
+        mainLabel.text = "\(seconds) 초"
+        
+        // number 변수에 초를 할당
+        number = seconds
     }
     
     @IBAction func startButtonTapped(_ sender: UIButton) {
         // 1초씩 지날 때마다 무언가를 실행
+        timer?.invalidate()
         
+        // withTimeInterval: 간격(초)
+        // 클로저 내에서 객체의 속성 및 메서드에 접근 시에는 "self"키워드를 반드시 사용
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) {
+            [self] _ in
+            // 반복하고 싶은 코드
+            
+            // 앞에서 [self] 를 작성해두면 self.number 이런식으로 작성하지 않아도 됨
+            if number > 0 {
+                number -= 1
+                
+                // 슬라이더 줄어듦 (계산해서 넣어야함)
+                slider.value = Float(number)/Float(60)
+                
+                // mainLabel 텍스트가 바뀜
+                mainLabel.text = "\(number) 초"
+            } else {
+                // number 값 초기화
+                number = 0
+                // mainLabel 텍스트
+                mainLabel.text = "알람!!!"
+                
+                // 초가 0이 됐을 시 소리(시스템 소리)
+                AudioServicesPlaySystemSound(SystemSoundID(1000))
+                
+                // 타이머 비활성화 (비활성화 하지 않으면 계속 1초 뒤 실행)
+                timer?.invalidate()
+            }
+        }
     }
     
     @IBAction func resetButtonTapped(_ sender: UIButton) {
-        // 슬라이더 가운데 설정
-        slider.value = 0.5
-        // slider.setValue(0.5, animated: true)
+        // 초기화
+        timer?.invalidate() // 타이머 비활성화
+        number = 0  // number 값 초기화
+        mainLabel.text = "초를 선택하세요" // mainLabel 텍스트 초기화
+        slider.value = 0.5  // 슬라이더 가운데 설정
     }
     
 }
