d03beb5f24fba816699c9c983048966e75adf3e4
Happy Hee <56466589+happy-hee@users.noreply.github.com>
2024-06-30 15:45:47 +0900

    첫번째 앱 만들기 - 코드 설정하기 (앱9강)

--- FirstNewApp/FirstNewApp/ViewController.swift
+++ FirstNewApp/FirstNewApp/ViewController.swift
@@ -11,27 +11,45 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var mainLabel: UILabel!
     
+    @IBOutlet weak var slider: UISlider!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // 기본 설정
         configureUI()
     }
 
+    
+    
     func configureUI() {
         mainLabel.text = "초를 선택하세요"
+        
+        // 슬라이더 가운데 설정
+        slider.value = 0.5  // value: 0~1
     }
     
+    // slider가 변할 때마다 실행
     @IBAction func sliderChanged(_ sender: UISlider) {
+        // 슬라이더의 value값을 가지고 mainLabel의 텍스트를 세팅
         
+        // 1) value값이 0~1로 되어있는걸 초로 바꾸기 위해 * 60을 함
+        // 2) 너무 디테일한 값으로 되어있으므로 정수(Int) 형태로 변형
+        let seconds = Int(slider.value * 60)
+        
+        mainLabel.text = "\(seconds)"
     }
     
     @IBAction func startButtonTapped(_ sender: UIButton) {
+        // 1초씩 지날 때마다 무언가를 실행
         
     }
     
     @IBAction func resetButtonTapped(_ sender: UIButton) {
-        
+        // 슬라이더 가운데 설정
+        slider.value = 0.5
+        // slider.setValue(0.5, animated: true)
     }
     
 }
