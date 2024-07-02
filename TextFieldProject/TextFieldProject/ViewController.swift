c308ddd005d22e898151b49fd7765042462804cf
Happy Hee <56466589+happy-hee@users.noreply.github.com>
2024-07-02 13:27:34 +0900

    두번째 앱 만들기 - 코드 설정하기(앱13강)

--- TextFieldProject/TextFieldProject/ViewController.swift
+++ TextFieldProject/TextFieldProject/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+    }
+    
+    // 기본 UI설정
+    func setupUI() {
+        view.backgroundColor = UIColor.gray
+        textField.keyboardType = UIKeyboardType.emailAddress    // 입력타입 -> 이메일
+        textField.placeholder = "이메일 입력"    // placeholder
+        textField.borderStyle = .roundedRect    // 가장자리 둥글게
+        textField.clearButtonMode = .always     // Clear 버튼 여부
+        textField.returnKeyType = .next     // return(엔터)키의 형태
     }
 
 
+    @IBAction func doneButtonTapped(_ sender: UIButton) {
+    }
+    
+    
 }
 
