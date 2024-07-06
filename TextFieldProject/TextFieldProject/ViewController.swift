6c44a0b0fa468c9d57d8107441ecb99dc19b7db9
Happy Hee <56466589+happy-hee@users.noreply.github.com>
2024-07-06 18:20:36 +0900

    두번째 앱 만들기 - 코드 설정하기(앱17강)

--- TextFieldProject/TextFieldProject/ViewController.swift
+++ TextFieldProject/TextFieldProject/ViewController.swift
@@ -16,12 +16,15 @@ class ViewController: UIViewController, UITextFieldDelegate {
        - 텍스트필드 프로토콜(델리게이트 프로토콜, 자격증)
      
      2) class UITextField       * RemoteControl
-       - 유저와 커뮤니케이션(유저가 입력 등) '동작'을 전달
-         예시) 유저가 입력을 시작함, 유저가 입력을 끝냄
+       - 유저와 커뮤니케이션(예시 - 유저가 입력했다는 '동작' 을 전달)
+       - 프로토콜, 델리게이트 패턴을 활용해서 ViewController 한테 이 '동작'을 전달
+         (textField의 대리자는 ViewController)
+       "텍스트필드에 입력이라는 '동작'이 일어나고 있어!"
      
-     3) class ViewController        * TV
+     3) class ViewController        * TV (RemoteControlDelegate 자격증을 채택하여 RemoteControl을 사용)
        - UITextField와 커뮤니케이션
        - 메서드 실행(구체적인 구현, 동작)
+       "입력이라는 '동작'이 일어난다고? 그럼 이렇게 할래(구체적인 구현)"
      */
     
 
@@ -33,6 +36,11 @@ class ViewController: UIViewController, UITextFieldDelegate {
         setupUI()
         
         textField.delegate = self   // delegate: 대리자 설정 -> 이 self(ViewController) 가 컨트롤러가 된다고 설정
+        
+        // 최초 응답자(responder) 설정
+        // 화면에 들어오면 텍스트필드가 가장 먼저 입력받는 상태가 됨
+        textField.becomeFirstResponder()    // 키보드가 올라옴
+        // textField.resignFirstResponder()
     }
     
     // 기본 UI설정
@@ -43,11 +51,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
         textField.borderStyle = .roundedRect    // 가장자리 둥글게
         textField.clearButtonMode = .always     // Clear 버튼 여부
         textField.returnKeyType = .next     // return(엔터)키의 형태
-    }
-
-    
-    @IBAction func doneButtonTapped(_ sender: UIButton) {
-        
     }
     
     
@@ -66,7 +69,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     // Clear 버튼 작동 여부
     func textFieldShouldClear(_ textField: UITextField) -> Bool {
         print(#function)
-        return false    // false로 해두면 x 버튼 눌러도 글자들이 지워지지 않음
+        return true    // false로 해두면 x 버튼 눌러도 글자들이 지워지지 않음
     }
     
     // 텍스트에 입력을 할 때마다 이 메서드를 불러옴
@@ -121,6 +124,17 @@ class ViewController: UIViewController, UITextFieldDelegate {
         print("입력 종료")
     }
     
+    // Done 버튼 탭
+    @IBAction func doneButtonTapped(_ sender: UIButton) {
+        // 응답자 제거
+        textField.resignFirstResponder()    // 키보드가 내려감
+    }
     
+    // ViewController에 이미 있는 touchesBegan 메서드를 재정의(override)
+    // 화면의 탭을 감지하는 메서드
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) { // 1) 터치가 일어나면
+        textField.resignFirstResponder()    // 2) 키보드를 내리겠다(키보드를 내리겠다)
+        // self.view.endEditing(true)   // 뷰를 끝내겠다(키보드도 내려감)
+    }
 }
 
