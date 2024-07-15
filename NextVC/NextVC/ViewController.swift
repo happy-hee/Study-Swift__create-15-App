eeabed18a1efce3b656866989c4e7542f8525909
Happy Hee <56466589+happy-hee@users.noreply.github.com>
2024-07-16 08:45:03 +0900

    화면 이동과 데이터 전달 (앱31강)

--- NextVC/NextVC/ViewController.swift
+++ NextVC/NextVC/ViewController.swift
@@ -32,11 +32,10 @@ class ViewController: UIViewController {
     
     // 2) 코드로 스토리보드 객체를 생성해서, 화면 이동
     @IBAction func storyboardWithCodeButtonTapped(_ sender: UIButton) {
-
-        
-        
-        
-        
+        guard let secondVC = storyboard?.instantiateViewController(withIdentifier: "secondVC") as? SecondViewController else { return }
+        secondVC.someString = "아빠상어"
+        secondVC.modalPresentationStyle = .fullScreen
+        present(secondVC, animated: true, completion: nil)
     }
     
     
