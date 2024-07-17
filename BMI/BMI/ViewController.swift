ca28536cd7dcffae33aed0a08d339078ef1c2e84
Happy Hee <56466589+happy-hee@users.noreply.github.com>
2024-07-17 11:02:23 +0900

    네번째 앱 만들기 - UI설정하기 (앱35강)

---
+++ BMI/BMI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI
+//
+//  Created by Nova on 2024/07/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
