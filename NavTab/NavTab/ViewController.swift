8a4d0a6a1f2c375ef843869076bad8ec53d4aa2a
Happy Hee <56466589+happy-hee@users.noreply.github.com>
2024-07-23 20:58:59 +0900

    여섯번째 앱 만들기 - 스토리보드로 네비게이션바/탭바 만들기 (앱 52강)

---
+++ NavTab/NavTab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavTab
+//
+//  Created by Nova on 2024/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // 1) item Scene -> Navigation bar -> 오른쪽에서 스크롤 어쩌구 체크해서 색 변경
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
