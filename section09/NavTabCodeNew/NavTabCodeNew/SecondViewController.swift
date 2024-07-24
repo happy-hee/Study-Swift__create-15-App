5140f3a4368daf8d0177b4c891f0cdc532b63694
Happy Hee <56466589+happy-hee@users.noreply.github.com>
2024-07-24 14:16:10 +0900

    여섯번째 앱 만들기 - 코드로 네비게이션바, 탭바 만들기 응용 및 정리 (앱 54강)

---
+++ section09/NavTabCodeNew/NavTabCodeNew/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  NavTabCodeNew
+//
+//  Created by 김다희 on 7/23/24.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+    }
+
+}
---
+++ section09/NavTabCodeNew/NavTabCodeNew/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavTabCodeNew
+//
+//  Created by 김다희 on 7/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
